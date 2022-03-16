6465940485e4a7b8c9da49310f92cf6d8c564169
Alberto-25 <ajst_95@outlook.com>
2022-03-16 17:45:13 -0500

    .env configurado

--- client-cine/src/components/App.js
+++ client-cine/src/components/App.js
@@ -4,13 +4,11 @@ import Review from './review/Review.js'
 
 export const App = () => {
   return (
-    // <BrowserRouter>
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/review' element={<Review />} />
         </Routes>
       </Router>
-    // </BrowserRouter>
   )
 }
